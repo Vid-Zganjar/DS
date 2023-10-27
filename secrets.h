19beffda6453cce8fa8d4e16eb56b573efe5a5ac
unknown <vz8898@student.uni-lj.si>
2023-10-27 12:02:57 +0200

    DS_VAJA3

---
+++ secrets.h
@@ -0,0 +1,9 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+#define SECRET_MAC {0x90, 0xA2, 0xDA, 0x10, 0x40, 0xD8}
+
+#define SECRET_CH_ID 2321550		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "9UU6TIWUZANAYKUG"   // replace XYZ with your channel write API Key
